1cf4950a7ed17fb6a8a8c888e022ea5aa5e91a17
mouheb-ria <mouhebabderrahim@gmail.com>
2022-04-06 22:54:49 +0100

    FIRST COMMUIT

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,58 +1,73 @@
-@extends('layouts.master')
-
-@section('title', 'Page de connexion')
+@extends('layouts.app')
 
 @section('content')
-    <div class="div-padding pt-5 signin-div user-access-bg">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-6 offset-lg-3 text-center">
-                    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-6 offset-lg-3">
-                    <div class="account-access sign-in">
-                        
-                        
-                        <div class="tab-content">
-                            
-                            <div role="tabpanel" class="tab-pane active show" id="driver">
-                                <form class="mb-4" method="POST" action="{{ route('login') }}">
-                                    @csrf
-                                    <div class="form-floating">
-                                      <input type="email"  name="email" class="form-control" id="floatingInputEmail" placeholder="name@example.com">
-                                      <label for="floatingInputEmail">Email address</label>
-                                      @error('email')
-                                          <span class="invalid-feedback d-block" role="alert">
-                                              <strong>{{ $message }}</strong>
-                                          </span>
-                                      @enderror
-                                    </div>
-                                    <div class="form-floating">
-                                      <input type="password" name="password" class="form-control" id="floatingPass" placeholder="Password">
-                                      <label for="floatingPass">Password</label>
-                                      @error('password')
-                                          <span class="invalid-feedback d-block" role="alert">
-                                              <strong>{{ $message }}</strong>
-                                          </span>
-                                      @enderror
-                                    </div>
-                                
-                                    <div class="checkbox mb-3">
-                                      <label>
-                                        <input type="checkbox" value="remember-me"> Remember me
-                                      </label>
-                                    </div>
-                                    <button type="submit" class="w-100 btn btn-lg btn-dark" type="submit">Connecter</button>
- 
-                                  </form>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Login') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <div class="row mb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                         </div>
-                    </div>
-                    
+
+                        <div class="row mb-3">
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
